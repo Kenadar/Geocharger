34e6e1a88758d843507fc34be360bd15b35e6ceb
Ssoonnnya <118048169+Ssoonnnya@users.noreply.github.com>
2023-06-01 14:44:37 +0000

    Created api endpoint to show all available points

--- routes/api.php
+++ routes/api.php
@@ -19,10 +19,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/geodata', function (Request $request){
+Route::get('/geodata/create', function (Request $request){
     Geodata::create([
         'name' => $request->get('name'),
         'latitude' => $request->get('latitude'),
         'longitude' => $request->get('longitude')
     ]);
+});
+
+Route::get('/geodata', function (Request $request){
+    $request = Geodata::all();
+    return $request -> toJson();
 });
