b7a8a0e7b96bebae04b670cf13c1d034a05d1b6d
Ssoonnnya <118048169+Ssoonnnya@users.noreply.github.com>
2023-06-26 13:34:39 +0000

    interval in 15 minutes(-already booked)

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -9,29 +9,29 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as Controller;
 use DateInterval;
 use DatePeriod;
+use DateTime;
 
 class BookingController extends Controller
 {
 
     public function store(Request $request): JsonResponse
     {
-        $validated = Validator::make($request->all(), [
+        $validated = Validator::make($request->all(), [ 
             'tenant' => 'required|integer',
             'lessor' => 'required|integer',
             'geodata_id' =>'required|integer',
-            'start_date' => 'required|date',
-            'end_date' => 'required|date',
-            'stary_time' => 'required|integer'
+            'start_time' => 'required|integer',
+            'end_time' => 'required|integer'
         ]);
-
 
         if ($validated->fails()) {
             return response()->json(['status'=> 'failed']);
         }
 
-        $alreadyBooked = Booking::where('geodata_id', '=',$request->get('geodata_id'))
-            ->where('date', '>=', $request->get('start_date'))
-            ->where('date', '<=', $request->get('end_date'))
+        $alreadyBooked = Booking::where('geodata_id', '=', $request->get('geodata_id'))
+ 
+            ->where('interval', '>=', $request->get('start_time'))
+            ->where('interval', '<=', $request->get('end_time'))
             ->get()->isNotEmpty();
             
 
@@ -39,27 +39,57 @@ class BookingController extends Controller
             return response()->json(['status'=>'already booked!']);
         }
 
-        /// create dateinterval
+        $start_time = new \DateTime();
+        $start_time->setTimestamp($request->get('start_time'));
 
-        $start_date = date_create($request->get('start_date'));
-        $end_date = date_create($request->get('end_date'))->modify('+1 day');
+        $end_time = new \DateTime();
+        $end_time->setTimestamp($request->get('end_time'));
 
-        $interval = DateInterval::createFromDateString('1 day');
-        $daterange = new DatePeriod($start_date, $interval , $end_date);
+        $interval = DateInterval::createFromDateString('15 minutes');
+        $daterange = new DatePeriod($start_time, $interval , $end_time);
 
         foreach($daterange as $date){
-            $date = $date->format('Y-m-d');
 
            Booking::create([
             'tenant'=> $request->get('tenant'),
             'lessor'=> $request->get('lessor'),
             'geodata_id'=> $request->get('geodata_id'),
-            'date'=>$date
+            'date'=> $this-> getDate($date),
+            'interval' => $this->getInterval($date)
         ]);
         }
 
+
         return response()->json(['status' => 'success']);
     } 
+
+    function roundMinutes($minutes) {
+        $remainder = $minutes % 15;
+        if ($remainder <= 7) {
+            $roundedMinutes = $minutes - $remainder;
+        } else {
+            $roundedMinutes = $minutes + (15 - $remainder);
+        }
+    
+        return $roundedMinutes;
+    }
+    
+    function getInterval(DateTime $dateTime): int|float
+    {
+        $hours = $dateTime->format('H');
+        $minutes = $dateTime->format('i');
+        $roundedMinutes = $this->roundMinutes($minutes);
+        $minutesFromDayStart = (($hours * 60) + $roundedMinutes);
+        $interval = $minutesFromDayStart / 15;
+        return $interval;
+    }
+    
+    function getDate(DateTime $dateTime) {
+        $date = $dateTime->format('Y-m-d');
+    
+        return $date;
+    }
+    
 
     }
 
