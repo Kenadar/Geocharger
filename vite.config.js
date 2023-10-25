06236af87d39af435d09577ae8b07540e48e2180
Sonya <SofiKonors@gmail.com>
2023-10-25 13:06:36 +0300

    done geoc-138

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,7 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/css/googleMap.css',
                 'resources/css/slider.css',
+                'resources/css/dashboardNav.css',
                 'resources/js/app.js',
                 'resources/js/dashboardMap.js'
             ],
